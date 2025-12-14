30a4e1fd00d94e86bc41e525d6fa9fbe61735aee
dks99 <dks998811@gmail.com>
2025-12-14 19:37:01 +0530

    Initial commit

---
+++ src/test/java/com/bonfire/kitchen/KitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bonfire.kitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
